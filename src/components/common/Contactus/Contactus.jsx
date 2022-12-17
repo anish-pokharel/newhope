ba52044f5ea5c2e0ac18d42c4fe0e67ec096c5e9
sunitadhikari <sunitaadhikari2001@gmail.com>
2022-12-17 23:43:24 +0545

    message

--- src/components/common/Contactus/Contactus.jsx
+++ src/components/common/Contactus/Contactus.jsx
@@ -4,7 +4,8 @@ import "./Contactus.css";
 const Contactus = () => {
   return (
     <div className="main_cont">
-      <h1>hello</h1>
+      <h1 className="h_cont">Want to Give any Advice?</h1>
+      <p className="p_cont">Call us on 1300 424 442 to talk about your needs. Or fill out the form below and our Care Advisors will be in touch soon.</p>
     </div>
   );
 };
