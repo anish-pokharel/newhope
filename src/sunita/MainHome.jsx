c6f745ef99d16cf8b723164310a2a81d219d19c0
sunitadhikari <sunitaadhikari2001@gmail.com>
2022-12-17 19:59:17 +0545

    message

--- src/sunita/MainHome.jsx
+++ src/sunita/MainHome.jsx
@@ -4,7 +4,11 @@ import "./MainHome.css";
 const MainHome = () => {
   return (
     <>
-      <h1>hello form me</h1>
+      <div className="outer">
+    <img src="new.jpeg"></img>
+      <h1>We Care With Respect</h1>
+      <p>Personalised Disability & Home Care Services.</p>
+      </div>
     </>
   );
 };
