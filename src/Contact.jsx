bb22e8c96c7a4ce56ded7930ee7abd75ad0d7d2d
Anish Pokharel <anish.pokharel.710.ap@gmail.com>
2022-12-17 19:57:41 +0545

    contact

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import "../src/styles/Contact.css";
 import Wrapper from "./components/common/Wrapper";
+import { ContactData } from "./dummydata/ContactData";
 
 const Contact = () => {
   return (
     <>
-      <div className="maincontainer1 contact">
+      <section className="maincontainer1 contact">
         <Wrapper title="contact us" subtitle="Contact" />
         <div className="contact-section">
           <h2>Ready To Learn More ?</h2>
@@ -13,8 +14,25 @@ const Contact = () => {
             Call us on 1300 424 442 to talk about your needs Or fill out the
             form below and our Care Advisors will be in touch soon.
           </p>
+          <div className="box">
+            {ContactData.map((val, index) => {
+              return (
+                <>
+                  <div className="flexSi" key={val.id}>
+                    <div className="logoSi">
+                      <i className={val.logo}></i>
+                    </div>
+                    <div className="text">
+                      <h2>{val.title}</h2>
+                      <p>{val.desc}</p>
+                    </div>
+                  </div>
+                </>
+              );
+            })}
+          </div>
         </div>
-      </div>
+      </section>
     </>
   );
 };
---
+++ src/dummydata/ContactData.jsx
@@ -0,0 +1,29 @@
+export const ContactData = [
+  {
+    id: 1,
+    logo: "fab fa-facebook-f icon   ",
+    title: "Address",
+    desc: "  3 Grumman Court, Zuccoli <br /> Northen Territory 0832 ",
+  },
+
+  {
+    id: 2,
+    logo: "fab fa-facebook-f icon   ",
+    title: "Phone Number",
+    desc: " 1300424442 ",
+  },
+
+  {
+    id: 3,
+    logo: "fab fa-facebook-f icon   ",
+    title: "    Mail",
+    desc: " info@hopeability.com.au ",
+  },
+
+  {
+    id: 4,
+    logo: "fab fa-facebook-f icon   ",
+    title: "We are Open",
+    desc: " monday-Friday:9AM-5 PM ",
+  },
+];
