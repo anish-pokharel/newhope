0cbef1a7b67d5ee2172cc7faa8ea73da68babc46
Anish Pokharel <anish.pokharel.710.ap@gmail.com>
2022-12-17 20:07:19 +0545

    sunita's error

--- src/sunita/MainHome.jsx
+++ src/sunita/MainHome.jsx
@@ -5,9 +5,11 @@ const MainHome = () => {
   return (
     <>
       <div className="outer">
-    <img src="new.jpeg"></img>
-      <h1>We Care With Respect</h1>
-      <p>Personalised Disability & Home Care Services.</p>
+        <img src="new.jpeg"></img>
+        <h1 className="homeheader">We Care With Respect</h1>
+        <p className="homepara">
+          Personalised Disability & Home Care Services.
+        </p>
       </div>
     </>
   );
