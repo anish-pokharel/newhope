8d8ecb2ce49449123e7bbafbfc61ff95daf061fc
Anish Pokharel <anish.pokharel.710.ap@gmail.com>
2022-12-20 00:38:57 +0545

    error

--- src/RenderPages/About.jsx
+++ src/RenderPages/About.jsx
@@ -5,62 +5,7 @@ import "../styles/About.css";
 const About = () => {
   return (
     <>
-      {/* <Wrapper title="About " subtitle="About" /> */}
-      <section className="container">
-        <div className="about-section">
-          <h1>Our values</h1>
-          <div className="flexSc">
-            <div className="aboutbox">
-              <div className="img">
-                <img src="" alt="" />
-              </div>
-              <div className="abouttext">
-                <h2>Honesty</h2>
-                <p>
-                  Lorem ipsum dolor sit, amet consectetur adipisicing elit. Esse
-                  incidunt quos temporibus.
-                </p>
-              </div>
-            </div>
-            <div className="aboutbox">
-              <div className="img">
-                <img src="" alt="" />
-              </div>
-              <div className="abouttext">
-                <h2>Honesty</h2>
-                <p>
-                  Lorem ipsum dolor sit, amet consectetur adipisicing elit. Esse
-                  incidunt quos temporibus.
-                </p>
-              </div>
-            </div>
-            <div className="aboutbox">
-              <div className="img">
-                <img src="" alt="" />
-              </div>
-              <div className="abouttext">
-                <h2>Honesty</h2>
-                <p>
-                  Lorem ipsum dolor sit, amet consectetur adipisicing elit. Esse
-                  incidunt quos temporibus.
-                </p>
-              </div>
-            </div>
-            <div className="aboutbox">
-              <div className="img">
-                <img src="" alt="" />
-              </div>
-              <div className="abouttext">
-                <h2>Honesty</h2>
-                <p>
-                  Lorem ipsum dolor sit, amet consectetur adipisicing elit. Esse
-                  incidunt quos temporibus.
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
+      <Wrapper title="About " subtitle="About" />
     </>
   );
 };
