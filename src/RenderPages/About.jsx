95c1cb6747ce4b2ef637f908203578de2d171b2c
Anish Pokharel <anish.pokharel.710.ap@gmail.com>
2022-12-20 00:35:51 +0545

    box css

--- src/RenderPages/About.jsx
+++ src/RenderPages/About.jsx
@@ -1,10 +1,66 @@
 import React from "react";
 import Wrapper from "../components/common/Wrapper";
+import "../styles/About.css";
 
 const About = () => {
   return (
     <>
-      <Wrapper title="About " subtitle="About" />
+      {/* <Wrapper title="About " subtitle="About" /> */}
+      <section className="container">
+        <div className="about-section">
+          <h1>Our values</h1>
+          <div className="flexSc">
+            <div className="aboutbox">
+              <div className="img">
+                <img src="" alt="" />
+              </div>
+              <div className="abouttext">
+                <h2>Honesty</h2>
+                <p>
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit. Esse
+                  incidunt quos temporibus.
+                </p>
+              </div>
+            </div>
+            <div className="aboutbox">
+              <div className="img">
+                <img src="" alt="" />
+              </div>
+              <div className="abouttext">
+                <h2>Honesty</h2>
+                <p>
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit. Esse
+                  incidunt quos temporibus.
+                </p>
+              </div>
+            </div>
+            <div className="aboutbox">
+              <div className="img">
+                <img src="" alt="" />
+              </div>
+              <div className="abouttext">
+                <h2>Honesty</h2>
+                <p>
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit. Esse
+                  incidunt quos temporibus.
+                </p>
+              </div>
+            </div>
+            <div className="aboutbox">
+              <div className="img">
+                <img src="" alt="" />
+              </div>
+              <div className="abouttext">
+                <h2>Honesty</h2>
+                <p>
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit. Esse
+                  incidunt quos temporibus.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </>
   );
 };
--- src/RenderPages/Work.jsx
+++ src/RenderPages/Work.jsx
@@ -17,15 +17,18 @@ const Work = () => {
           </p>
         </div>
         <div className="nextpara">
-           <h1 className="work-head">Current Vacancies</h1>
-           <p className="work-newp">There are no current vacancies. Please check again or register your interest below and we’ll be in touch when suitable positions become available.</p>
-           
-        <div className="photooo">
+          <h1 className="work-head">Current Vacancies</h1>
+          <p className="work-newp">
+            There are no current vacancies. Please check again or register your
+            interest below and we’ll be in touch when suitable positions become
+            available.
+          </p>
+
+          <div className="photooo"></div>
         </div>
-        </div>
-               <div className="new-formm"></div>
+        <div className="new-formm"></div>
       </section>
     </>
-    );
+  );
 };
 export default Work;
