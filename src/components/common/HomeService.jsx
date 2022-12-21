2bf7ae84632ecd83bc0ed0a6ee23842084c99a64
Anish Pokharel <anish.pokharel.710.ap@gmail.com>
2022-12-21 21:05:51 +0545

    jgkj

--- src/components/common/HomeService.jsx
+++ src/components/common/HomeService.jsx
@@ -22,4 +22,12 @@ const HomeService = () => {
   );
 };
 
+<p>
+  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Soluta, inventore
+  minus odio consectetur minima voluptatem eveniet illum ipsam ratione
+  voluptates ex qui sunt quisquam! Consequatur voluptatibus, reiciendis possimus
+  accusamus dicta placeat quam, quae neque rem nulla totam ex suscipit inventore
+  hic quo laboriosam expedita. Eum deleniti modi id accusantium totam?
+</p>;
+
 export default HomeService;
