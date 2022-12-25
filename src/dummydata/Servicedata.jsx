813b0b76ece354ab46c5625e0c410db752ab2425
sunitadhikari <sunitaadhikari2001@gmail.com>
2022-12-25 21:25:08 +0545

    jg

--- src/dummydata/Servicedata.jsx
+++ src/dummydata/Servicedata.jsx
@@ -2,21 +2,21 @@ export const ServiceData = [
   {
     id: 1,
     cover:
-      "https://images.pexels.com/photos/114907/pexels-photo-114907.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-    title: "Development-Life ",
+      "neww.jpg",
+      title: "Development-Life ",
     desc: "Development of daily living and life skills focuses on training and development activities.",
   },
   {
     id: 2,
     cover:
-      "https://images.pexels.com/photos/114907/pexels-photo-114907.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+      "APA.jpg",
     title: "Respect",
     desc: "Assisting with and/or supervising personal tasks of daily life to enable the participant to.",
   },
   {
     id: 3,
     cover:
-      "https://images.pexels.com/photos/114907/pexels-photo-114907.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+      "baack.webp",
     title: "Assistance Life Tasks",
     desc: "This registration group incorporates assistance with or supervising tasks of daily lifes.",
   },
