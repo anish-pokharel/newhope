e80f712ebc45eec9999c6ffffef808e6e03b4247
Anish Pokharel <anish.pokharel.710.ap@gmail.com>
2022-12-25 20:17:46 +0545

    service design complete

--- src/RenderPages/Service.jsx
+++ src/RenderPages/Service.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import Wrapper from "../components/common/Wrapper";
 // import BoxSection from "../components/CommonRender/BoxSection";
 import MainBox from "../components/CommonRender/MainBox";
@@ -10,62 +11,63 @@ const Service = () => {
     <>
       <>
         <Wrapper title=" Service" subtitle="Service" />
-        <section className="service-cont">
-          {ServiceBox.map((val) => {
-            return (
-              <>
-                <div className="ser-two" key={val.id}>
-                  <h1 className="ser-he">{val.title}</h1>
-                  <div className="pho">
-                    <img src={val.cover} alt="" />
-                  </div>
-                  <button id="myButtn">Get Started</button>
-                  <p className="servpp">{val.p}</p>
-                  {val.subTopic.map((details, index) => {
-                    return (
-                      <>
-                        <div className="listt" key={index}>
-                          <ol>
-                            <li>
-                              <strong>{details.subtitle1}</strong>
-                              <br />
-                              {details.subtitleinfo1}
-                            </li>
-                            <li>
-                              <strong>{details.subtitle2}</strong>
-                              <br />
-                              {details.subtitleinfo2}
-                            </li>
-                            <li>
-                              <strong>{details.subtitle3}</strong>
-                              <br />
-                              {details.subtitleinfo3}
-                            </li>
-                            <li>
-                              <strong>{details.subtitle4}</strong>
-                              <br />
-                              {details.subtitleinfo4}
-                            </li>
-                            <li>
-                              <strong>{details.subtitle5} </strong>
-                              <br />
-                              {details.subtitleinfo5}
-                            </li>
-                            <li>
-                              <strong>{details.subtitle6}</strong>
-                              <br />
-                              {details.subtitleinfo6}
-                            </li>
-                          </ol>
-                        </div>
-                      </>
-                    );
-                  })}
+        {ServiceBox.map((val) => {
+          return (
+            <>
+              <div className="ser-two" key={val.id}>
+                <h1 className="ser-he">{val.title}</h1>
+                <div className="pho">
+                  <img src={val.cover} alt="" />
                 </div>
-              </>
-            );
-          })}
-        </section>
+                <NavLink to="/Contact">
+                  <button id="myButtn">Get Started</button>
+                </NavLink>
+                <p className="servpp">{val.p}</p>
+
+                {val.subTopic.map((details, index) => {
+                  return (
+                    <>
+                      <div className="listt" key={index}>
+                        <ol>
+                          <li>
+                            <strong>{details.subtitle1}</strong>
+                            {/* <br /> */}
+                            {details.subtitleinfo1}
+                          </li>
+                          <li>
+                            <strong>{details.subtitle2}</strong>
+                            {/* <br /> */}
+                            {details.subtitleinfo2}
+                          </li>
+                          <li>
+                            <strong>{details.subtitle3}</strong>
+                            {/* <br /> */}
+                            {details.subtitleinfo3}
+                          </li>
+                          <li>
+                            <strong>{details.subtitle4}</strong>
+                            {/* <br /> */}
+                            {details.subtitleinfo4}
+                          </li>
+                          <li>
+                            <strong>{details.subtitle5} </strong>
+                            {/* <br /> */}
+                            {details.subtitleinfo5}
+                          </li>
+                          <li>
+                            <strong>{details.subtitle6}</strong>
+                            {/* <br /> */}
+                            {details.subtitleinfo6}
+                          </li>
+                        </ol>
+                      </div>
+                    </>
+                  );
+                })}
+              </div>
+            </>
+          );
+        })}
         {/*
         <ServiceData />
         <div className="container-box grid grid-four-column">
--- src/dummydata/Servicedata.jsx
+++ src/dummydata/Servicedata.jsx
@@ -31,24 +31,20 @@ export const ServiceBox = [
     p: " We help with and/or supervise personal daily life tasks to enable you to live as autonomously as possible. We provide individual support in a range of environments, including but not limited to, your own home. We’ll assist you with your daily life, whether you only need a little help getting your day started or more ongoing support. Our services are highly personalized to meet your unique requirements ",
     subTopic: [
       {
-        subtitle1: " Getting Around",
+        subtitle1: " Getting Around : ",
         subtitleinfo1:
           "Assisting you in moving around the house, such as transfers from bed or chair to wheelchair or toilet.",
-        subtitle2: " Getting Dressed",
+        subtitle2: " Getting Dressed : ",
         subtitleinfo2:
-          "Assisting you in moving around the house, such as transfers from bed or chair to wheelchair or toilet.",
-        subtitle3: " Bathroom Assistance",
+          "Assisting you in getting dressed and ready for the day each morning with hoist assistance (if required).",
+        subtitle3: " Bathroom Assistance : ",
         subtitleinfo3:
-          "Assisting you in moving around the house, such as transfers from bed or chair to wheelchair or toilet.",
-        subtitle4: " Getting Around",
+          "Assisting you with your personal hygiene, such as showering, toileting, brushing your teeth or shaving",
+        subtitle4: "Medication Assistance : ",
         subtitleinfo4:
-          "Assisting you in moving around the house, such as transfers from bed or chair to wheelchair or toilet.",
-        subtitle5: " Getting Around",
-        subtitleinfo5:
-          "Assisting you in moving around the house, such as transfers from bed or chair to wheelchair or toilet.",
-        subtitle6: " Getting Around",
-        subtitleinfo6:
-          "Assisting you in moving around the house, such as transfers from bed or chair to wheelchair or toilet.",
+          "Assisting you in taking the correct medication at the right time.",
+        subtitle5: " Cooking assistance and meal preparation:        ",
+        subtitle6: "Laundry and ironing: ",
       },
     ],
   },
@@ -56,9 +52,33 @@ export const ServiceBox = [
     id: 2,
     cover:
       "https://images.pexels.com/photos/114907/pexels-photo-114907.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-    title: "Daily Personal Activities",
+    title: "Development Of Daily Living And Life Skills",
 
-    p: " We help with and/or supervise personal daily life tasks to enable you to live as autonomously as possible. We provide individual support in a range of environments, including but not limited to, your own home. We’ll assist you with your daily life, whether you only need a little help getting your day started or more ongoing support. Our services are highly personalized to meet your unique requirements ",
+    p: "We focus on training and development activities to increase your ability to live as autonomously as possible to develop your daily living and life skills. We provide support to enhance your ability to travel and use public transport independently. ",
+    subTopic: [
+      {
+        subtitleinfo1:
+          "Support to join local exercise classes and sporting clubs, social groups aligned to your cultural preferences and your interests, visiting local community centers. ",
+        subtitleinfo2: "Relaxation techniques, eating out.          ",
+        subtitleinfo3:
+          "Personal presentation, hygiene and personal care, laundry, and clothes care.          ",
+        subtitleinfo4:
+          "Proactive behavior and speaking up for yourself, developing relationships.          ",
+        subtitleinfo5:
+          "Paying bills, developing, and following a budget, money skills, learning about renting, literacy and numeracy. ",
+        subtitleinfo6:
+          "Preparing a healthy weekly menu, shopping for groceries, cooking, and healthy eating. ",
+      },
+    ],
+  },
+  {
+    id: 3,
+    cover:
+      "https://images.pexels.com/photos/114907/pexels-photo-114907.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    title: "Assistance With Travel/Transport Arrangements",
+
+    p: " We are passionate about helping you to be as independent as possible by providing safe and reliable transport to get to and from appointments and whatever else you need to do in your community. If getting around independently is your goal, then our friendly staff can help you build capacity to use public transport safely and with confidence. ",
+
     subTopic: [
       {
         subtitle1: " Transport Service",
@@ -76,36 +96,7 @@ export const ServiceBox = [
       },
     ],
   },
-  {
-    id: 3,
-    cover:
-      "https://www.hopeability.com.au/wp-content/uploads/2021/03/Assistance-with-Travel-Image-1.webp",
-    title: "Assistance With Travel/Transport Arrangements",
 
-    p: " We are passionate about helping you to be as independent as possible by providing safe and reliable transport to get to and from appointments and whatever else you need to do in your community. If getting around independently is your goal, then our friendly staff can help you build capacity to use public transport safely and with confidence.",
-    subTopic: [
-      {
-        subtitle1: " Getting Around",
-        subtitleinfo1:
-          "Assisting you in moving around the house, such as transfers from bed or chair to wheelchair or toilet.",
-        subtitle2: " Getting Dressed",
-        subtitleinfo2:
-          "Assisting you in moving around the house, such as transfers from bed or chair to wheelchair or toilet.",
-        subtitle3: " Bathroom Assistance",
-        subtitleinfo3:
-          "Assisting you in moving around the house, such as transfers from bed or chair to wheelchair or toilet.",
-        subtitle4: " Getting Around",
-        subtitleinfo4:
-          "Assisting you in moving around the house, such as transfers from bed or chair to wheelchair or toilet.",
-        subtitle5: " Getting Around",
-        subtitleinfo5:
-          "Assisting you in moving around the house, such as transfers from bed or chair to wheelchair or toilet.",
-        subtitle6: " Getting Around",
-        subtitleinfo6:
-          "Assisting you in moving around the house, such as transfers from bed or chair to wheelchair or toilet.",
-      },
-    ],
-  },
   {
     id: 4,
     cover:
@@ -127,6 +118,29 @@ export const ServiceBox = [
         subtitle4: " Gardening & Lawn Care",
         subtitleinfo4:
           "Keeping your garden and lawn healthy and looking great from season to season. ",
+      },
+    ],
+  },
+  {
+    id: 5,
+    cover:
+      "https://images.pexels.com/photos/114907/pexels-photo-114907.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    title:
+      "Assistance With Daily Life Tasks In A Group Or Shared Living Arrangement",
+
+    p: " We provide assistance with and/or supervising tasks of daily life in a shared living environment, which is either temporary or ongoing, with a focus on developing your skills to live as autonomously as possible. This includes short-term accommodation and assistance .",
+
+    subTopic: [
+      {
+        subtitle1: " Lower Needs",
+        subtitleinfo1:
+          " Typically, not 24/7, providing supervision of living arrangements to ensure your home is right for you.",
+        subtitle2: " Standard Needs",
+        subtitleinfo2:
+          " Providing active assistance and supervision for most daily tasks, and 24/7 support. ",
+        subtitle3: " Higher Needs",
+        subtitleinfo3:
+          "Highly frequent assistance, active assistance with all daily tasks, and support in managing challenging behaviors. ",
       },
     ],
   },
