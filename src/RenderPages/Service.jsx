b1a135314e8dbd4004f5a099e6b9a62879b26d0d
Anish Pokharel <anish.pokharel.710.ap@gmail.com>
2022-12-25 18:51:22 +0545

    service

--- src/RenderPages/Service.jsx
+++ src/RenderPages/Service.jsx
@@ -3,16 +3,75 @@ import Wrapper from "../components/common/Wrapper";
 // import BoxSection from "../components/CommonRender/BoxSection";
 import MainBox from "../components/CommonRender/MainBox";
 import ServiceData from "../components/CommonRender/ServiceData";
+import { ServiceBox } from "../dummydata/Servicedata";
 
 const Service = () => {
   return (
     <>
       <>
         <Wrapper title=" Service" subtitle="Service" />
-        {/* <ServiceData/>
+        <section className="service-cont">
+          {ServiceBox.map((val) => {
+            return (
+              <>
+                <div className="ser-two" key={val.id}>
+                  <h1 className="ser-he">{val.title}</h1>
+                  <div className="pho">
+                    <img src={val.cover} alt="" />
+                  </div>
+                  <button id="myButtn">Get Started</button>
+                  <p className="servpp">{val.p}</p>
+                  {val.subTopic.map((details, index) => {
+                    return (
+                      <>
+                        <div className="listt" key={index}>
+                          <ul>
+                            <li>
+                              <strong>{details.subtitle}</strong>
+
+                              {details.subtitleinfo}
+                            </li>
+                            <li>
+                              <strong>{details.subtitle2}</strong>
+                              <br />
+                              {details.subtitleinfo2}
+                            </li>
+                            <li>
+                              <strong>{details.subtitle3}</strong>
+                              <br />
+                              {details.subtitleinfo3}
+                            </li>
+                            <li>
+                              <strong>{details.subtitle4}</strong>
+                              <br />
+                              {details.subtitleinfo4}
+                            </li>
+                            <li>
+                              <strong>{details.subtitle5} </strong>
+                              <br />
+                              {details.subtitleinfo5}
+                            </li>
+                            <li>
+                              <strong>{details.subtitle6}</strong>
+                              <br />
+                              {details.subtitleinfo6}
+                            </li>
+                          </ul>
+                        </div>
+                      </>
+                    );
+                  })}
+                </div>
+              </>
+            );
+          })}
+        </section>
+        {/*
+        <ServiceData />
         <div className="container-box grid grid-four-column">
            <MainBox /> 
-        </div> */}
+        </div>
+          */}
       </>
     </>
   );
--- src/dummydata/Servicedata.jsx
+++ src/dummydata/Servicedata.jsx
@@ -24,104 +24,108 @@ export const ServiceData = [
 export const ServiceBox = [
   {
     id: 1,
-    cover: "htttps://",
+    cover:
+      "https://images.pexels.com/photos/114907/pexels-photo-114907.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
     title: "Daily Personal Activities",
 
     p: " We help with and/or supervise personal daily life tasks to enable you to live as autonomously as possible. We provide individual support in a range of environments, including but not limited to, your own home. We’ll assist you with your daily life, whether you only need a little help getting your day started or more ongoing support. Our services are highly personalized to meet your unique requirements ",
-    subtopic: [
+    subTopic: [
       {
-        subtitle: " Getting Around",
-        subtitleinfo:
+        subtitle1: " Getting Around",
+        subtitleinfo1:
           "Assisting you in moving around the house, such as transfers from bed or chair to wheelchair or toilet.",
-        subtitle: " Getting Dressed",
-        subtitleinfo:
+        subtitle2: " Getting Dressed",
+        subtitleinfo2:
           "Assisting you in moving around the house, such as transfers from bed or chair to wheelchair or toilet.",
-        subtitle: " Bathroom Assistance",
-        subtitleinfo:
+        subtitle3: " Bathroom Assistance",
+        subtitleinfo3:
           "Assisting you in moving around the house, such as transfers from bed or chair to wheelchair or toilet.",
-        subtitle: " Getting Around",
-        subtitleinfo:
+        subtitle4: " Getting Around",
+        subtitleinfo4:
           "Assisting you in moving around the house, such as transfers from bed or chair to wheelchair or toilet.",
-        subtitle: " Getting Around",
-        subtitleinfo:
+        subtitle5: " Getting Around",
+        subtitleinfo5:
           "Assisting you in moving around the house, such as transfers from bed or chair to wheelchair or toilet.",
-        subtitle: " Getting Around",
-        subtitleinfo:
+        subtitle6: " Getting Around",
+        subtitleinfo6:
           "Assisting you in moving around the house, such as transfers from bed or chair to wheelchair or toilet.",
       },
     ],
   },
   {
     id: 2,
-    cover: "htttps://",
+    cover:
+      "https://images.pexels.com/photos/114907/pexels-photo-114907.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
     title: "Daily Personal Activities",
 
     p: " We help with and/or supervise personal daily life tasks to enable you to live as autonomously as possible. We provide individual support in a range of environments, including but not limited to, your own home. We’ll assist you with your daily life, whether you only need a little help getting your day started or more ongoing support. Our services are highly personalized to meet your unique requirements ",
-    subtopic: [
+    subTopic: [
       {
-        subtitle: " Transport Service",
-        subtitleinfo:
+        subtitle1: " Transport Service",
+        subtitleinfo1:
           "Getting out and engaging with friends and the community is just as important as enjoying your home environment. We’ll help to ensure you are able to lead a life that’s active and meaningful. ",
-        subtitle: " Community Access",
-        subtitleinfo:
+        subtitle2: " Community Access",
+        subtitleinfo2:
           "Helping you get out into your community and into your local town or city by going with you on public transport. ",
-        subtitle: " Social Activities",
-        subtitleinfo:
+        subtitle3: " Social Activities",
+        subtitleinfo3:
           "Helping you try new things or continue existing activities whilst making friends – from hobbies, recreation, leisure, sports, and arts to community programs like gardening or volunteering. ",
-        subtitle: " Appointments",
-        subtitleinfo:
+        subtitle4: " Appointments",
+        subtitleinfo4:
           "Getting you to and from your medical, school or job appointments. ",
       },
     ],
   },
   {
     id: 3,
-    cover: "htttps://",
+    cover:
+      "https://www.hopeability.com.au/wp-content/uploads/2021/03/Assistance-with-Travel-Image-1.webp",
     title: "Assistance With Travel/Transport Arrangements",
 
     p: " We are passionate about helping you to be as independent as possible by providing safe and reliable transport to get to and from appointments and whatever else you need to do in your community. If getting around independently is your goal, then our friendly staff can help you build capacity to use public transport safely and with confidence.",
-    subtopic: [
+    subTopic: [
       {
-        subtitle: " Getting Around",
-        subtitleinfo:
+        subtitle1: " Getting Around",
+        subtitleinfo1:
           "Assisting you in moving around the house, such as transfers from bed or chair to wheelchair or toilet.",
-        subtitle: " Getting Dressed",
-        subtitleinfo:
+        subtitle2: " Getting Dressed",
+        subtitleinfo2:
           "Assisting you in moving around the house, such as transfers from bed or chair to wheelchair or toilet.",
-        subtitle: " Bathroom Assistance",
-        subtitleinfo:
+        subtitle3: " Bathroom Assistance",
+        subtitleinfo3:
           "Assisting you in moving around the house, such as transfers from bed or chair to wheelchair or toilet.",
-        subtitle: " Getting Around",
-        subtitleinfo:
+        subtitle4: " Getting Around",
+        subtitleinfo4:
           "Assisting you in moving around the house, such as transfers from bed or chair to wheelchair or toilet.",
-        subtitle: " Getting Around",
-        subtitleinfo:
+        subtitle5: " Getting Around",
+        subtitleinfo5:
           "Assisting you in moving around the house, such as transfers from bed or chair to wheelchair or toilet.",
-        subtitle: " Getting Around",
-        subtitleinfo:
+        subtitle6: " Getting Around",
+        subtitleinfo6:
           "Assisting you in moving around the house, such as transfers from bed or chair to wheelchair or toilet.",
       },
     ],
   },
   {
     id: 4,
-    cover: "htttps://",
+    cover:
+      "https://images.pexels.com/photos/114907/pexels-photo-114907.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
     title: "Household Tasks",
 
     p: " Having a clean and neat home environment is important for wellbeing, and our friendly support staff can help you with tasks such as cleaning, laundry, shopping, dishwashing, meal preparation and home maintenance.We provide domestic and personal assistance, as well as home maintenance assistance, to ensure that your home is safe and accessible. ",
-    subtopic: [
+    subTopic: [
       {
-        subtitle: " Home Maintenance",
-        subtitleinfo:
+        subtitle1: " Home Maintenance",
+        subtitleinfo1:
           " We can help you get to things that can be hard to reach-like changing light bulbs or smoke alarm batteries.",
-        subtitle: " House Cleaning",
-        subtitleinfo:
+        subtitle2: " House Cleaning",
+        subtitleinfo2:
           "Tidying, washing dishes and helping to maintain a clean home for you to relax in. ",
-        subtitle: " Minor Repairs",
-        subtitleinfo:
+        subtitle3: " Minor Repairs",
+        subtitleinfo3:
           "Taking care of fixtures and fittings such as taps and paint chips that naturally wear out over time and require repairs and servicing. ",
-        subtitle: " Gardening & Lawn Care",
-        subtitleinfo:
+        subtitle4: " Gardening & Lawn Care",
+        subtitleinfo4:
           "Keeping your garden and lawn healthy and looking great from season to season. ",
       },
     ],
