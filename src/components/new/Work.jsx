31115d0c339e62e5ce0f0306bdcff553bb4f2bfb
sunitadhikari <sunitaadhikari2001@gmail.com>
2022-12-27 19:23:20 +0545

    kgjj

---
+++ src/components/new/Work.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+
+export const Work = () => {
+  return (
+    <>
+        <div class="work-one">
+            <h1 class="work-onee">Work With Us</h1>
+            <p class="work-oneee">Home » Work With Us</p>
+        </div>
+        <div class="new-secc">
+            <p class="new-seccc">We are always on the lookout for the best Disability Support Workers and Community Nurses. Our culture is all about creating a safe space for the whole team. Our values are respect, inclusion, and friendly support. If you think you’d be a great fit, we would love to hear from you.</p>
+        </div>
+        <div class="work-two">
+            <h1 class="work-twoo">Current Vacancies</h1>
+            <p class="work-twooo">There are no current vacancies. Please check again or register your interest below and we’ll be in touch when suitable positions become available.</p>
+            <div class="work-twoooo"></div>
+        </div>
+        <div class="work-three">
+            <form class="formm">
+                <h1 class="work-form">Join The Hopeability Team</h1>
+                <p class="work-formm">Please register your interest below and we’ll be in touch soon.</p>
+                <p class="workfp">"*" indicates required fields</p>
+                <label class="fnamee" for="fname" > Name*</label>
+                <input type="text" id="fname" name="name"><br>
+                <label class="femaill" for="email">Enter your Email*</label>
+               <input type="email" id="emaiil" name="email"><br>
+               <label class="fphonee" for="phone">Phone*</label>
+               <input type="tel" id="phonee" name="phone" pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}">
+               <label class="ffile" for="file">Upload CV*</label>
+               <input type="file" id="myFile" name="filename"><br>
+               <p class="maxx">Max. file size: 8 MB.</p>
+               <label class="ftextt" for="text"> Tell Us About Your Experience:</label>
+               <textarea rows="4" cols="50" name="comment" form="usrform" id="textare"></textarea>
+               <input class="submitt" type="submit" value="Submit">
+            </form>
+        </div>
+    </>
+  )
+}
