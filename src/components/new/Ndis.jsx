5af89bb57d73ad7717a0ad2f24af80ab77590400
Anish Pokharel <anish.pokharel.710.ap@gmail.com>
2022-12-27 20:27:51 +0545

    eroor fix

--- src/components/new/Ndis.jsx
+++ src/components/new/Ndis.jsx
@@ -1,135 +1,135 @@
-<<<<<<< HEAD
 import React from "react";
 
- const Ndis = () => {
+const Ndis = () => {
   return (
-  <>
-  
-  <div className="naidsmain">
+    <>
+      <div className="naidsmain">
         <h1 className="naidh">Registered NDIS Provider</h1>
         <p className="naidp">Home » Registered NDIS Provider</p>
-=======
- import React from 'react'
- 
- export const ndis = () => {
-   return (
-    <>
-        <div class="naidsmain">
-        <h1 class="naidh">Registered NDIS Provider</h1>
-        {/* <h1 class="nah">Registed NDIS Provider</h1> */}
-        <p class="naidp">Home » Registered NDIS Provider</p>
->>>>>>> 3c5d6c623eb3d18c1c44666c17ac7e5573364cb8
-        </div>
-        <div className="naidnex">
-            <p className="naidsp">The below information is about NDIS, how it works, who qualifies for NDIS, what funding is available and what can it be spent on, careers as a support worker. If you have any questions, please asks for more information.
-              <br/>
-              <br/>
-              <br/>                
-              We hope you find this as helpful as we did, a lot of information about NDIS can be found from the button below</p>
-              </div>
-              <div className="video">
-              <iframe class ="video1" src="https://www.youtube.com/embed/qZOjPBJiBPg" title="What is the NDIS?" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      </div>
+      <div className="naidnex">
+        <p className="naidsp">
+          The below information is about NDIS, how it works, who qualifies for
+          NDIS, what funding is available and what can it be spent on, careers
+          as a support worker. If you have any questions, please asks for more
+          information.
+          <br />
+          <br />
+          <br />
+          We hope you find this as helpful as we did, a lot of information about
+          NDIS can be found from the button below
+        </p>
+      </div>
+      <div className="video">
+        <iframe
+          class="video1"
+          src="https://www.youtube.com/embed/qZOjPBJiBPg"
+          title="What is the NDIS?"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen
+        ></iframe>
 
-      <iframe
-        className="video1"
-        src="https://www.youtube.com/embed/qZOjPBJiBPg"
-        title="What is the NDIS?"
-        frameborder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-        allowfullscreen
-      ></iframe>
+        <iframe
+          className="video1"
+          src="https://www.youtube.com/embed/qZOjPBJiBPg"
+          title="What is the NDIS?"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen
+        ></iframe>
 
-      <iframe
-        className="video2"
-        src="https://www.youtube.com/embed/aF3gGRNRnkk"
-        title="Accessing the NDIS"
-        frameborder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-        allowfullscreen
-      ></iframe>
+        <iframe
+          className="video2"
+          src="https://www.youtube.com/embed/aF3gGRNRnkk"
+          title="Accessing the NDIS"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen
+        ></iframe>
 
-      <iframe
-        className="video3"
-        src="https://www.youtube.com/embed/Ts8AqzsgK2Q"
-        title="The NDIS Explained - NDIS Funding Categories"
-        frameborder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-        allowfullscreen
-      ></iframe>
+        <iframe
+          className="video3"
+          src="https://www.youtube.com/embed/Ts8AqzsgK2Q"
+          title="The NDIS Explained - NDIS Funding Categories"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen
+        ></iframe>
 
-      <iframe
-        className="video4"
-        src="https://www.youtube.com/embed/dqvLhbzru1Q"
-        title="What Happens During a NDIS Planning Meeting?"
-        frameborder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-        allowfullscreen
-      ></iframe>
+        <iframe
+          className="video4"
+          src="https://www.youtube.com/embed/dqvLhbzru1Q"
+          title="What Happens During a NDIS Planning Meeting?"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen
+        ></iframe>
 
-      <iframe
-        className="video5"
-        src="https://www.youtube.com/embed/0KT3g3sf6-I"
-        title="How to Manage Your NDIS Plan - Overview"
-        frameborder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-        allowfullscreen
-      ></iframe>
+        <iframe
+          className="video5"
+          src="https://www.youtube.com/embed/0KT3g3sf6-I"
+          title="How to Manage Your NDIS Plan - Overview"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen
+        ></iframe>
 
-      <iframe
-        className="video6"
-        src="https://www.youtube.com/embed/qZOjPBJiBPg"
-        title="What is the NDIS?"
-        frameborder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-        allowfullscreen
-      ></iframe>
+        <iframe
+          className="video6"
+          src="https://www.youtube.com/embed/qZOjPBJiBPg"
+          title="What is the NDIS?"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen
+        ></iframe>
 
-      <iframe
-        className="video7"
-        src="https://www.youtube.com/embed/2TqfrBdC-74"
-        title="What Can You Spend Your Funding On"
-        frameborder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-        allowfullscreen
-      ></iframe>
+        <iframe
+          className="video7"
+          src="https://www.youtube.com/embed/2TqfrBdC-74"
+          title="What Can You Spend Your Funding On"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen
+        ></iframe>
 
-      <iframe
-        className="video8"
-        src="https://www.youtube.com/embed/VSbHanbKlAs"
-        title="What is support coordination?"
-        frameborder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-        allowfullscreen
-      ></iframe>
+        <iframe
+          className="video8"
+          src="https://www.youtube.com/embed/VSbHanbKlAs"
+          title="What is support coordination?"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen
+        ></iframe>
 
-      <iframe
-        className="video9"
-        src="https://www.youtube.com/embed/XuFWxMXkG4o"
-        title="Webinar: How to get the most out of your NDIS plan"
-        frameborder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-        allowfullscreen
-      ></iframe>
+        <iframe
+          className="video9"
+          src="https://www.youtube.com/embed/XuFWxMXkG4o"
+          title="Webinar: How to get the most out of your NDIS plan"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen
+        ></iframe>
 
-      <iframe
-        className="video10"
-        src="https://www.youtube.com/embed/cObGJAWit2w"
-        title="Pathways to Employment"
-        frameborder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-        allowfullscreen
-      ></iframe>
+        <iframe
+          className="video10"
+          src="https://www.youtube.com/embed/cObGJAWit2w"
+          title="Pathways to Employment"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen
+        ></iframe>
 
-      <iframe
-        className="video11"
-        src="https://www.youtube.com/embed/hPNA0WROnqE"
-        title="NDIS Disability Support Worker: Gregg Sinnamon"
-        frameborder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-        allowfullscreen
-      ></iframe>
-  
-  </>
+        <iframe
+          className="video11"
+          src="https://www.youtube.com/embed/hPNA0WROnqE"
+          title="NDIS Disability Support Worker: Gregg Sinnamon"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen
+        ></iframe>
+      </div>
+    </>
   );
 };
 
