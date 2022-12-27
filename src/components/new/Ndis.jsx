d432892c125a961ec0399c9794947704719b8291
Anish Pokharel <anish.pokharel.710.ap@gmail.com>
2022-12-27 20:17:49 +0545

    pushh error

--- src/components/new/Ndis.jsx
+++ src/components/new/Ndis.jsx
@@ -1,25 +1,25 @@
 import React from "react";
 
-export const Ndis = () => {
+ const Ndis = () => {
   return (
   <>
   
-  <div class="naidsmain">
-        <h1 class="naidh">Registered NDIS Provider</h1>
-        <p class="naidp">Home » Registered NDIS Provider</p>
+  <div className="naidsmain">
+        <h1 className="naidh">Registered NDIS Provider</h1>
+        <p className="naidp">Home » Registered NDIS Provider</p>
         </div>
-        <div class="naidnex">
-            <p class="naidsp">The below information is about NDIS, how it works, who qualifies for NDIS, what funding is available and what can it be spent on, careers as a support worker. If you have any questions, please asks for more information.
+        <div className="naidnex">
+            <p className="naidsp">The below information is about NDIS, how it works, who qualifies for NDIS, what funding is available and what can it be spent on, careers as a support worker. If you have any questions, please asks for more information.
               <br/>
               <br/>
               <br/>                
               We hope you find this as helpful as we did, a lot of information about NDIS can be found from the button below</p>
               </div>
-              <div class="video">
+              <div className="video">
               <iframe class ="video1" src="https://www.youtube.com/embed/qZOjPBJiBPg" title="What is the NDIS?" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 
       <iframe
-        class="video1"
+        className="video1"
         src="https://www.youtube.com/embed/qZOjPBJiBPg"
         title="What is the NDIS?"
         frameborder="0"
@@ -28,7 +28,7 @@ export const Ndis = () => {
       ></iframe>
 
       <iframe
-        class="video2"
+        className="video2"
         src="https://www.youtube.com/embed/aF3gGRNRnkk"
         title="Accessing the NDIS"
         frameborder="0"
@@ -37,7 +37,7 @@ export const Ndis = () => {
       ></iframe>
 
       <iframe
-        class="video3"
+        className="video3"
         src="https://www.youtube.com/embed/Ts8AqzsgK2Q"
         title="The NDIS Explained - NDIS Funding Categories"
         frameborder="0"
@@ -46,7 +46,7 @@ export const Ndis = () => {
       ></iframe>
 
       <iframe
-        class="video4"
+        className="video4"
         src="https://www.youtube.com/embed/dqvLhbzru1Q"
         title="What Happens During a NDIS Planning Meeting?"
         frameborder="0"
@@ -55,7 +55,7 @@ export const Ndis = () => {
       ></iframe>
 
       <iframe
-        class="video5"
+        className="video5"
         src="https://www.youtube.com/embed/0KT3g3sf6-I"
         title="How to Manage Your NDIS Plan - Overview"
         frameborder="0"
@@ -64,7 +64,7 @@ export const Ndis = () => {
       ></iframe>
 
       <iframe
-        class="video6"
+        className="video6"
         src="https://www.youtube.com/embed/qZOjPBJiBPg"
         title="What is the NDIS?"
         frameborder="0"
@@ -73,7 +73,7 @@ export const Ndis = () => {
       ></iframe>
 
       <iframe
-        class="video7"
+        className="video7"
         src="https://www.youtube.com/embed/2TqfrBdC-74"
         title="What Can You Spend Your Funding On"
         frameborder="0"
@@ -82,7 +82,7 @@ export const Ndis = () => {
       ></iframe>
 
       <iframe
-        class="video8"
+        className="video8"
         src="https://www.youtube.com/embed/VSbHanbKlAs"
         title="What is support coordination?"
         frameborder="0"
@@ -91,7 +91,7 @@ export const Ndis = () => {
       ></iframe>
 
       <iframe
-        class="video9"
+        className="video9"
         src="https://www.youtube.com/embed/XuFWxMXkG4o"
         title="Webinar: How to get the most out of your NDIS plan"
         frameborder="0"
@@ -100,7 +100,7 @@ export const Ndis = () => {
       ></iframe>
 
       <iframe
-        class="video10"
+        className="video10"
         src="https://www.youtube.com/embed/cObGJAWit2w"
         title="Pathways to Employment"
         frameborder="0"
@@ -109,7 +109,7 @@ export const Ndis = () => {
       ></iframe>
 
       <iframe
-        class="video11"
+        className="video11"
         src="https://www.youtube.com/embed/hPNA0WROnqE"
         title="NDIS Disability Support Worker: Gregg Sinnamon"
         frameborder="0"
@@ -117,7 +117,8 @@ export const Ndis = () => {
         allowfullscreen
       ></iframe>
   
-  </>);
+  </>
+  );
 };
 
 export default Ndis;
--- src/components/new/Work.jsx
+++ src/components/new/Work.jsx
@@ -1,43 +1,78 @@
 import React from "react";
 
- const Work = () => {
-    return (
+const Work = () => {
+  return (
     <>
-  
-
-        <div className="work-one">
-            <h1 className="work-onee">Work With Us</h1>
-            <p className="work-oneee">Home » Work With Us</p>
-        </div>
-        <div className="new-secc">
-            <p className="new-seccc">We are always on the lookout for the best Disability Support Workers and Community Nurses. Our culture is all about creating a safe space for the whole team. Our values are respect, inclusion, and friendly support. If you think you'd be a great fit, we would love to hear from you.</p>
-        </div>
-        <div className="work-two">
-            <h1 className="work-twoo">Current Vacancies</h1>
-            <p className="work-twooo">There are no current vacancies. Please check again or register your interest below and we'll be in touch when suitable positions become available.</p>
-            <div className="work-twoooo"></div>
-        </div>
-        <div className="work-three">
-            <form className="formm">
-                <h1 className="work-form">Join The Hopeability Team</h1>
-                <p className="work-formm">Please register your interest below and we'll be in touch soon.</p>
-                <p className="workfp">"*" indicates required fields</p>
-                <label className="fnamee" for="fname" > Name*</label>
-                <input type="text" id="fname" name="name"/><br/>
-                <label className="femaill" for="email">Enter your Email*</label>
-               <input type="email" id="emaiil" name="email"/><br/>
-               <label className="fphonee" for="phone">Phone*</label>
-               <input type="tel" id="phonee" name="phone" pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}"/>
-               <label className="ffile" for="file">Upload CV*</label>
-               <input type="file" id="myFile" name="filename"/><br/>
-               <p className="maxx">Max. file size: 8 MB.</p>
-               <label className="ftextt" for="text"> Tell Us About Your Experience:</label>
-               <textarea rows="4" cols="50" name="comment" form="usrform" id="textare"></textarea>
-               <input className="submitt" type="submit" value="Submit"/>
-            </form>
-        </div>
-   
-    <>
+      <div className="work-one">
+        <h1 className="work-onee">Work With Us</h1>
+        <p className="work-oneee">Home » Work With Us</p>
+      </div>
+      <div className="new-secc">
+        <p className="new-seccc">
+          We are always on the lookout for the best Disability Support Workers
+          and Community Nurses. Our culture is all about creating a safe space
+          for the whole team. Our values are respect, inclusion, and friendly
+          support. If you think you'd be a great fit, we would love to hear from
+          you.
+        </p>
+      </div>
+      <div className="work-two">
+        <h1 className="work-twoo">Current Vacancies</h1>
+        <p className="work-twooo">
+          There are no current vacancies. Please check again or register your
+          interest below and we'll be in touch when suitable positions become
+          available.
+        </p>
+        <div className="work-twoooo"></div>
+      </div>
+      <div className="work-three">
+        <form className="formm">
+          <h1 className="work-form">Join The Hopeability Team</h1>
+          <p className="work-formm">
+            Please register your interest below and we'll be in touch soon.
+          </p>
+          <p className="workfp">"*" indicates required fields</p>
+          <label className="fnamee" for="fname">
+            {" "}
+            Name*
+          </label>
+          <input type="text" id="fname" name="name" />
+          <br />
+          <label className="femaill" for="email">
+            Enter your Email*
+          </label>
+          <input type="email" id="emaiil" name="email" />
+          <br />
+          <label className="fphonee" for="phone">
+            Phone*
+          </label>
+          <input
+            type="tel"
+            id="phonee"
+            name="phone"
+            pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}"
+          />
+          <label className="ffile" for="file">
+            Upload CV*
+          </label>
+          <input type="file" id="myFile" name="filename" />
+          <br />
+          <p className="maxx">Max. file size: 8 MB.</p>
+          <label className="ftextt" for="text">
+            {" "}
+            Tell Us About Your Experience:
+          </label>
+          <textarea
+            rows="4"
+            cols="50"
+            name="comment"
+            form="usrform"
+            id="textare"
+          ></textarea>
+          <input className="submitt" type="submit" value="Submit" />
+        </form>
+      </div>
+    </>
   );
 };
-export default Work ;
+export default Work;
