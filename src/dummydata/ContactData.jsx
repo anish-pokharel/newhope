119f2a195f518794e68728fd0e2c4f9b3b930b05
Anish Pokharel <anish.pokharel.710.ap@gmail.com>
2023-01-04 23:10:45 +0545

    icons

--- src/dummydata/ContactData.jsx
+++ src/dummydata/ContactData.jsx
@@ -1,28 +1,28 @@
 export const ContactData = [
   {
     id: 1,
-    logo:  "far fa-location-dot icon   ",
+    logo: "fa fa-map-marker-alt icon  ",
     title: "Address",
     desc: "  3 Grumman Court, Zuccoli Northen Territory 0832 ",
   },
 
   {
     id: 2,
-    logo: "fab fa-facebook-f icon   ",
+    logo: "fa fa-phone icon   ",
     title: "Phone Number",
     desc: " 1300424442 ",
   },
 
   {
     id: 3,
-    logo: "fab fa-facebook-f icon   ",
+    logo: "fa fa-envelope icon   ",
     title: "    Mail",
     desc: " info@hopeability.com.au ",
   },
 
   {
     id: 4,
-    logo: "fab fa-facebook-f icon   ",
+    logo: "fas fa-door-open icon   ",
     title: "We are Open",
     desc: " monday-Friday:9AM-5 PM ",
   },
